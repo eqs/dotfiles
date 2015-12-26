9086ceb9a0140219d2dd0b816dfab4f648947ca7
eqs <tikuwatabetai@gmail.com>
2015-12-26 14:56:31 +0900

    Add .latexmkrc

--- makesymlink.sh
+++ makesymlink.sh
@@ -1,2 +1,3 @@
 ln -s "/home/chikuwa/dotfiles/.vimrc" "/home/chikuwa/.vimrc" 
 ln -s "/home/chikuwa/dotfiles/.gvimrc" "/home/chikuwa/.gvimrc" 
+ln -s "/home/chikuwa/dotfiles/.latexmkrc" "/home/chikuwa/.latexmkrc" 
