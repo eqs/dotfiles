29b85e77206a123a078703de75140b938f99b279
eqs <i11murasige@gmail.com>
2017-12-01 09:14:20 +0900

    Add : xonshrc

--- makesymlink.sh
+++ makesymlink.sh
@@ -3,3 +3,4 @@ ln -s ~/dotfiles/.gvimrc    ~/.gvimrc
 ln -s ~/dotfiles/.latexmkrc ~/.latexmkrc 
 ln -s ~/dotfiles/.vimperatorrc ~/.vimperatorrc 
 ln -s ~/dotfiles/.inputrc ~/.inputrc 
+ln -s ~/dotfiles/.xonshrc ~/.xonshrc 
